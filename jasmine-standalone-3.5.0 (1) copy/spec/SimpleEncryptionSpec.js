2181832a0476a0670ebfafc3493fe3e9186e8b09
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2021-03-11 21:00:26 +0100

    passing test with logic

--- jasmine-standalone-3.5.0 (1) copy/spec/SimpleEncryptionSpec.js
+++ jasmine-standalone-3.5.0 (1) copy/spec/SimpleEncryptionSpec.js
@@ -7,7 +7,7 @@ describe("Encrypter", () => {
 
 	describe("#encrypt", () => {
 		it('should return "hsi  etTi sats!" when passed "This is a test!"', () => {
-			expect(encrypter.encrypt("This is a test", 1)).toEqual(
+			expect(encrypter.encrypt("This is a test!", 1)).toEqual(
 				"hsi  etTi sats!"
 			);
 		});
--- jasmine-standalone-3.5.0 (1) copy/src/SimpleEncryption.js
+++ jasmine-standalone-3.5.0 (1) copy/src/SimpleEncryption.js
@@ -2,6 +2,20 @@ class Encrypter {
 	constructor() {}
 
 	encrypt(message, number) {
-		return "hsi  etTi sats!";
+		let container = [];
+
+		for (let i = 0; i < message.length; i++) {
+			if (i % 2 !== 0) {
+				container.push(message[i]);
+			}
+		}
+		console.log(container);
+		for (let i = 0; i < message.length; i++) {
+			if (i % 2 === 0) {
+				container.push(message[i]);
+			}
+		}
+
+		return container.join("");
 	}
 }
